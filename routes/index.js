6acc06a5942cf189fbb0cebf8140809a0dfe6c67
Darilee2604 <88863409+LiscyViera@users.noreply.github.com>
2023-03-13 09:38:39 -0600

    etiquetado pacas
    nuevo

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'ETIQUETA FUNDAS DE CAPA' });
+});
+
+router.get('/resumen', function(req, res, next) {
+  res.render('resumen', { title: 'RESUMEN ETIQUETA FUNDAS DE CAPA' });
+});
+
+router.get('/registro', function(req, res, next) {
+  res.render('register', { title: 'REGISTRAR NUEVA ETIQUETA DE FUNDAS' });
+});
+
+module.exports = router;
