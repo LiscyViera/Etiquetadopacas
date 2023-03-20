7c32b964ead8d2556ec2c71bdac5b75c8553027c
Mafer42 <89046041+Mafer42@users.noreply.github.com>
2023-03-20 16:53:08 -0600

    pacas
    rutas

--- app.js
+++ app.js
@@ -37,5 +37,39 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+var mysql = require('mysql');
 
-module.exports = app;
+var conexion = mysql.createConnection({
+  host: 'localhost',
+  database: 'prueba_db',
+  user: 'root',
+  password: 'password'
+});
+
+conexion.connect((error) =>{
+  if (error) {
+    console.error('Error al conectar a la base de datos: ' + error.stack);
+    return;
+  } else {
+    console.log('Conexión a la base de datos exitosa.');
+  }
+
+});
+ 
+app.post('/registro', (req, res) => {
+  const { n_paca, variedad, clase, peso_humedo, peso_despalillo, gavillas_funda, gavilla_paca, prom_gavillas, n_tickets, fecha_elaboracion } = req.body;
+  const query = `INSERT INTO ticket_gavilla (n_paca, variedad, clase, peso_humedo, peso_despalillo, gavillas_funda, gavilla_paca, prom_gavillas, n_tickets, fecha_elaboracion) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+  connection.query(query, [n_paca, variedad, clase, peso_humedo, peso_despalillo, gavillas_funda, gavilla_paca, prom_gavillas, n_tickets, fecha_elaboracion], (error, res) => {
+    if (error) {
+      console.log('Error al insertar datos:', error);
+      res.status(500).send('Error al insertar datos');
+    } else {
+      console.log('Datos insertados correctamente');
+      res.status(200).send('Datos insertados correctamente');
+    }
+  });
+});
+
+// Iniciar servidor en el puerto 3000
+conexion.end();
+module.exports = conexion, app;
--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,11 @@ router.get('/registro', function(req, res, next) {
   res.render('register', { title: 'REGISTRAR NUEVA ETIQUETA DE FUNDAS' });
 });
 
+const bodyParser = require('body-parser');
+
+router.post('/registro', bodyParser.urlencoded({ extended: true }), (req, res, next) => {
+  const datosFormulario = req.body;
+  req.render('registrar', { title: 'NUEVA ETIQUETA DE FUNDAS REGISTRADA' });
+  // Procesa los datos del formulario y envía la respuesta correspondiente
+});
 module.exports = router;
